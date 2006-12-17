424bb1a4d49c45464d9ea444866cb12ddc672066
Dunwael <Dunwael@8d4f32b5-7f24-0410-a711-1f11bf6ed558>
2006-12-17 03:31:56 +0000

    Tentative de lua-isation d'une Frame (SpellTimerButton)

--- XML/XML.lua
+++ XML/XML.lua
@@ -31,3 +31,49 @@
 -- Version 15.12.2006-1
 ------------------------------------------------------------------------------------------------------
 
+-- Création du bouton de Timer des sorts
+local SpellTimerButton = CreateFrame
+	(
+		"Button",
+		"NecrosisSpellTimerButton",
+		UIParent,
+		"SecureActionButtonTemplate"
+	);
+
+SpellTimerButton:SetFrameStrata("MEDIUM");
+SpellTimerButton:SetMovable(true);
+SpellTimerButton:EnableMouse(true);
+SpellTimerButton:SetMovable(true);
+SpellTimerButton:SetWidth(34);
+SpellTimerButton:SetHeight(34);
+SpellTimerButton:SetNormalTexture("Interface\\AddOns\\Necrosis\\UI\\SpellTimerButton-Normal");
+SpellTimerButton:SetPushedTexture("Interface\\AddOns\\Necrosis\\UI\\SpellTimerButton-Pushed");
+SpellTimerButton:SetHighlightTexture("Interface\\AddOns\\Necrosis\\UI\\SpellTimerButton-Highlight");
+SpellTimerButton:Hide();
+
+-- Création de la liste des Timers Textes
+local TextTimer = SpellTimerButton:CreateFontString
+	(
+		"NecrosisListSpells",
+		nil,
+		"GameFontNormalSmall"
+	);
+
+TextTimer:SetJustifyH("LEFT");
+TextTimer:SetPoint("LEFT", "NecrosisSpellTimerButton", "LEFT", 23, 0);
+TextTimer:SetTextColor(1, 1, 1);
+
+-- Edition des scripts associés au bouton
+SpellTimerButton:SetScript
+	("OnLoad", function()
+		this:RegisterForDrag("LeftButton");
+		this:RegisterForClicks("RightButtonUp");
+	end);
+SpellTimerButton:SetScript
+	("OnEnter", function()
+		Necrosis_BuildTooltip(this, "SpellTimer", AnchorSpellTimerTooltip);
+	end);
+SpellTimerButton:SetScript("OnLeave", function() GameTooltip:Hide(); end);
+SpellTimerButton:SetScript("OnMouseUp", function() Necrosis_OnDragStop(this); end);
+SpellTimerButton:SetScript("OnDragStart", function() Necrosis_OnDragStart(this); end);
+SpellTimerButton:SetScript("OnDragStop", function() Necrosis_OnDragStop(this); end);
