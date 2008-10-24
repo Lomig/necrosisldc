54191c309e7407bda0c172f29a70851560a51f34
Dunwael <Dunwael@8d4f32b5-7f24-0410-a711-1f11bf6ed558>
2008-10-24 09:21:59 +0000

    * Milestone

---
+++ tags/3.0 RC1/Bindings.lua
@@ -0,0 +1,133 @@
+﻿--[[
+    Necrosis LdC
+    Copyright (C) 2005-2008  Lom Enfroy
+
+    This file is part of Necrosis LdC.
+
+    Necrosis LdC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Necrosis LdC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Necrosis LdC; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--]]
+
+
+------------------------------------------------------------------------------------------------------
+-- Necrosis LdC
+-- Par Lomig (Kael'Thas EU/FR) & Tarcalion (Nagrand US/Oceanic) 
+-- Contributions deLiadora et Nyx (Kael'Thas et Elune EU/FR)
+--
+-- Skins et voix Françaises : Eliah, Ner'zhul
+--
+-- Version Allemande par Geschan
+-- Version Espagnole par DosS (Zul’jin)
+-- Version Russe par Komsomolka
+--
+-- Version $LastChangedDate$
+------------------------------------------------------------------------------------------------------
+
+
+-- On définit G comme étant le tableau contenant toutes les frames existantes.
+local _G = getfenv(0)
+
+-- Création des raccourcis claviers
+function Necrosis:BindName()
+
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Première section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	-- Le Titre
+	_G["BINDING_HEADER_Necrosis"] = "Necrosis LdC"
+
+	-- La Sphere
+	_G["BINDING_NAME_CLICK NecrosisButton:LeftButton"] = Necrosis.Spell[NecrosisConfig.MainSpell].Name
+
+	-- Le Cheval
+	if NecrosisConfig.OwnMount then
+		local _, nameMount1 = GetCompanionInfo("MOUNT", NecrosisConfig.LeftMount)
+		local _, nameMount2 = GetCompanionInfo("MOUNT", NecrosisConfig.RightMount)
+		_G["BINDING_NAME_CLICK NecrosisMountButton:LeftButton"] = nameMount1
+		_G["BINDING_NAME_CLICK NecrosisMountButton:RightButton"] = nameMount2
+	elseif Necrosis.Spell[2].ID then
+		_G["BINDING_NAME_CLICK NecrosisMountButton:LeftButton"] = Necrosis.Spell[2].Name
+		_G["BINDING_NAME_CLICK NecrosisMountButton:RightButton"] = Necrosis.Spell[1].Name
+	else
+		_G["BINDING_NAME_CLICK NecrosisMountButton:LeftButton"] = Necrosis.Spell[1].Name
+		_G["BINDING_NAME_CLICK NecrosisMountButton:RightButton"] = Necrosis.Spell[1].Name
+	end
+
+	-- La Pierre de feu
+	_G["BINDING_NAME_CLICK NecrosisFirestoneButton:RightButton"] = Necrosis.Spell[54].Name
+	_G["BINDING_NAME_CLICK NecrosisFirestoneButton:LeftButton"] = self.Translation.Item.Firestone
+
+	--La Pierre de sort
+	_G["BINDING_NAME_CLICK NecrosisSpellstoneButton:RightButton"] = Necrosis.Spell[53].Name
+	_G["BINDING_NAME_CLICK NecrosisSpellstoneButton:LeftButton"] = self.Translation.Item.Spellstone
+
+	--La Pierre de soin
+	_G["BINDING_NAME_CLICK NecrosisHealthstoneButton:RightButton"] = Necrosis.Spell[52].Name
+	_G["BINDING_NAME_CLICK NecrosisHealthstoneButton:LeftButton"] = self.Translation.Item.Healthstone.." - "..NECROSIS_COOLDOWN["Utilisation"]
+	_G["BINDING_NAME_CLICK NecrosisHealthstoneButton:MiddleButton"] = self.Translation.Item.Healthstone.." - "..NECROSIS_COOLDOWN["Echange"]
+
+	-- La Pierre de sort
+	_G["BINDING_NAME_CLICK NecrosisSoulstoneButton:RightButton"] = Necrosis.Spell[51].Name
+	_G["BINDING_NAME_CLICK NecrosisSoulstoneButton:LeftButton"] = self.Translation.Item.Soulstone
+	
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Deuxième section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	_G["BINDING_HEADER_NecrosisSpell"] = "Necrosis - "..Necrosis.Config.Menus["Menu des Buffs"]
+	
+	_G["BINDING_NAME_CLICK NecrosisBuffMenuButton:LeftButton"] = Necrosis.Config.Menus["Menu des Buffs"]
+	
+	local buffID = {31, 47, 32, 33, 34, 37, 39, 38, 43}
+	for i = 1, #buffID, 1 do
+		_G["BINDING_NAME_CLICK NecrosisBuffMenu"..i..":LeftButton"] = Necrosis.Spell[ buffID[i] ].Name
+	end
+	
+	_G["BINDING_NAME_CLICK NecrosisBuffMenu11:LeftButton"] = Necrosis.Spell[9].Name
+	_G["BINDING_NAME_CLICK NecrosisBuffMenu11:RightButton"] = Necrosis.Spell[9].Name.." (1)"
+
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Troisième section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	_G["BINDING_HEADER_NecrosisPet"] = "Necrosis - "..Necrosis.Config.Menus["Menu des Demons"]
+	
+	_G["BINDING_NAME_CLICK NecrosisPetMenuButton:LeftButton"] = Necrosis.Config.Menus["Menu des Demons"]
+	
+	for i = 2, 6, 1 do
+		_G["BINDING_NAME_CLICK NecrosisPetMenu"..i..":LeftButton"] = Necrosis.Spell[i+1].Name
+	end
+	
+	local buttonID = {1, 7, 8, 9, 10}
+	local BuffID = {15, 8, 30, 35, 44}
+	for i = 1, #buttonID, 1 do
+		_G["BINDING_NAME_CLICK NecrosisPetMenu"..buttonID[i]..":LeftButton"] = Necrosis.Spell[ BuffID[i] ].Name
+	end
+
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Quatrième section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	-- Le Titre
+	_G["BINDING_HEADER_NecrosisCurse"] = "Necrosis - "..Necrosis.Config.Menus["Menu des Maledictions"]
+	
+	_G["BINDING_NAME_CLICK NecrosisCurseMenuButton:LeftButton"] = Necrosis.Config.Menus["Menu des Maledictions"]
+	
+	local buffID = {23, 22, 24, 25, 40, 26, 16}
+	for i = 1, #buffID, 1 do
+		_G["BINDING_NAME_CLICK NecrosisCurseMenu"..i..":LeftButton"] = Necrosis.Spell[ buffID[i] ].Name
+	end
+
+end
---
+++ tags/3.0 RC1/Spells.lua
@@ -0,0 +1,107 @@
+﻿--[[
+    Necrosis LdC
+    Copyright (C) 2005-2008  Lom Enfroy
+
+    This file is part of Necrosis LdC.
+
+    Necrosis LdC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Necrosis LdC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Necrosis LdC; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--]]
+
+
+------------------------------------------------------------------------------------------------------
+-- Necrosis LdC
+-- Par Lomig (Kael'Thas EU/FR) & Tarcalion (Nagrand US/Oceanic) 
+-- Contributions deLiadora et Nyx (Kael'Thas et Elune EU/FR)
+--
+-- Skins et voix Françaises : Eliah, Ner'zhul
+--
+-- Version Allemande par Geschan
+-- Version Espagnole par DosS (Zul’jin)
+-- Version Russe par Komsomolka
+--
+-- Version $LastChangedDate$
+------------------------------------------------------------------------------------------------------
+
+-- Fonction pour éviter de devoir relocaliser la base des sorts disponibles
+function Necrosis:SpellLocalize()
+	self.Spell = {
+		[1] = {Name = GetSpellInfo(5784), 				Length = 0,		Type = 0}, -- Felsteed
+		[2] = {Name = GetSpellInfo(23161), 			 	Length = 0,		Type = 0}, -- Dreadsteed
+		[3] = {Name = GetSpellInfo(688), 				Length = 0,		Type = 0}, -- Diablotin || Imp
+		[4] = {Name = GetSpellInfo(697),				Length = 0,		Type = 0}, -- Marcheur || Voidwalker
+		[5] = {Name = GetSpellInfo(712),				Length = 0,		Type = 0}, -- Succube || Succubus
+		[6] = {Name = GetSpellInfo(691),				Length = 0,		Type = 0}, -- Fellhunter
+		[7] = {Name = GetSpellInfo(30146),				Length = 0,		Type = 0}, -- Felguard
+		[8] = {Name = GetSpellInfo(1122),				Length = 1200,	Type = 3}, -- Infernal
+		[9] = {Name = GetSpellInfo(710),				Length = 30,	Type = 2}, -- Banish
+		[10] = {Name = GetSpellInfo(1098),				Length = 300,	Type = 2}, -- Enslave
+		[11] = {Name = GetSpellInfo(27239),				Length = 1800,	Type = 1}, -- Résurrection de pierre d'ame || Soulstone Resurrection
+		[12] = {Name = GetSpellInfo(47811),				Length = 15,	Type = 6}, -- Immolation || Immolate
+		[13] = {Name = GetSpellInfo(6215),				Length = 15,	Type = 6}, -- Peur || Fear
+		[14] = {Name = GetSpellInfo(47813),				Length = 18,	Type = 5}, -- Corruption
+		[15] = {Name = GetSpellInfo(18708),				Length = 900,	Type = 3}, -- Domination corrompue || Fel Domination
+		[16] = {Name = GetSpellInfo(47867),				Length = 60,	Type = 3}, -- Malédiction funeste || Curse of Doom
+		[17] = {Name = GetSpellInfo(47847),				Length = 20,	Type = 3}, -- Furie de l'ombre || Shadowfury
+		[18] = {Name = GetSpellInfo(47825),				Length = 60,	Type = 3}, -- Feu de l'âme || Soul Fire
+		[19] = {Name = GetSpellInfo(47860),				Length = 120,	Type = 3}, -- Voile mortel || Death Coil
+		[20] = {Name = GetSpellInfo(47827),				Length = 15,	Type = 3}, -- Brûlure de l'ombre || Shadowburn
+		[21] = {Name = GetSpellInfo(47829),				Length = 10,	Type = 3}, -- Conflagration
+		[22] = {Name = GetSpellInfo(47864),				Length = 24,	Type = 4}, -- Malédiction Agonie || Curse of Agony
+		[23] = {Name = GetSpellInfo(50511),				Length = 120,	Type = 4}, -- Malédiction Faiblesse || Curse of Weakness
+		[24] = {Name = GetSpellInfo(57595),				Length = 120,	Type = 4}, -- Malédiction Témérité || Curse of Recklessness
+		[25] = {Name = GetSpellInfo(11719),				Length = 30,	Type = 4}, -- Malédiction Langage || Curse of Tongues
+		[26] = {Name = GetSpellInfo(47865),				Length = 300,	Type = 4}, -- Malédiction Eléments || Curse of the Elements
+		[27] = {Name = GetSpellInfo(59673),				Length = 180,	Type = 1}, -- Metamorphose || Metamorphosis
+		[28] = {Name = GetSpellInfo(47862),				Length = 30,	Type = 6}, -- Syphon de vie || Siphon Life
+		[29] = {Name = GetSpellInfo(17928),				Length = 40,	Type = 3}, -- Hurlement de terreur || Howl of Terror
+		[30] = {Name = GetSpellInfo(18540),				Length = 3600,	Type = 0}, -- Rituel funeste || Ritual of Doom
+		[31] = {Name = GetSpellInfo(47889),				Length = 0,		Type = 0}, -- Armure démoniaque || Demon Armor
+		[32] = {Name = GetSpellInfo(5697),				Length = 0,		Type = 0}, -- Respiration interminable || Unending Breath
+		[33] = {Name = GetSpellInfo(132),				Length = 0,		Type = 0}, -- Détection de l'invisibilité || Detect Invisibility
+		[34] = {Name = GetSpellInfo(126),				Length = 0,		Type = 0}, -- Oeil de Kilrogg || Eye of Kilrogg
+		[35] = {Name = GetSpellInfo(1098),				Length = 0,		Type = 0}, -- Enslave Demon
+		[36] = {Name = GetSpellInfo(696),				Length = 0,		Type = 0}, -- Peau de démon || Demon Skin
+		[37] = {Name = GetSpellInfo(698),				Length = 0,		Type = 0}, -- Rituel d'invocation || Ritual of Summoning
+		[38] = {Name = GetSpellInfo(19028),				Length = 0,		Type = 0}, -- Lien spirituel || Soul Link
+		[39] = {Name = GetSpellInfo(5500),				Length = 0,		Type = 0}, -- Détection des démons || Sense Demons
+		[40] = {Name = GetSpellInfo(18223),				Length = 12,	Type = 4}, -- Malédiction de fatigue || Curse of Exhaustion
+		[41] = {Name = GetSpellInfo(57946),				Length = 0,		Type = 0}, -- Connexion || Life Tap
+		[42] = {Name = GetSpellInfo(59164),				Length = 12,	Type = 2}, -- Hanter || Haunt
+		[43] = {Name = GetSpellInfo(47891),				Length = 30,	Type = 3}, -- Gardien de l'ombre || Shadow Ward
+		[44] = {Name = GetSpellInfo(18788),				Length = 0,		Type = 0}, -- Sacrifice démoniaque || Demonic Sacrifice
+		[45] = {Name = GetSpellInfo(47809),				Length = 0,		Type = 0}, -- Shadow Bolt
+		[46] = {Name = GetSpellInfo(47843),				Length = 18,	Type = 6}, -- Affliction instable || Unstable Affliction
+		[47] = {Name = GetSpellInfo(47893),				Length = 0,		Type = 0}, -- Gangrarmure || Fel Armor
+		[48] = {Name = GetSpellInfo(47836),				Length = 18,	Type = 5}, -- Graine de Corruption || Seed of Corruption
+		[49] = {Name = GetSpellInfo(29858),				Length = 300,	Type = 3}, -- Brise âme || SoulShatter
+		[50] = {Name = GetSpellInfo(58887),				Length = 300,	Type = 3}, -- Rituel des âmes || Ritual of Souls
+		[51] = {Name = GetSpellInfo(47884),				Length = 0,		Type = 0}, -- Création pierre d'âme || Create Soulstone
+		[52] = {Name = GetSpellInfo(47878),				Length = 0,		Type = 0}, -- Création pierre de soin || Create Healthstone
+		[53] = {Name = GetSpellInfo(47888),				Length = 0,		Type = 0}, -- Création pierre de sort || Create Spellstone
+		[54] = {Name = GetSpellInfo(60220),				Length = 0,		Type = 0}, -- Création pierre de feu || Create Firestone
+		[55] = {Name = GetSpellInfo(59092),				Length = 0,		Type = 0}, -- Pacte noir || Dark Pact
+	}
+	-- Type 0 = Pas de Timer || no timer
+	-- Type 1 = Timer permanent principal || Standing main timer
+	-- Type 2 = Timer permanent || main timer
+	-- Type 3 = Timer de cooldown || cooldown timer
+	-- Type 4 = Timer de malédiction || curse timer
+	-- Type 5 = Timer de corruption || corruption timer
+	-- Type 6 = Timer de combat || combat timer
+
+	for i in ipairs(Necrosis.Spell) do
+		Necrosis.Spell[i].Rank = " "
+	end
+end
