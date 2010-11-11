2a01af01526f37b422b9f3bae7f0b971485ccdeb
dunwael <dunwael@8d4f32b5-7f24-0410-a711-1f11bf6ed558>
2010-11-11 06:30:44 +0000

    First Alpha to be tagged as well. I will not tell what's wrong, but what's right. ANYTHING THAT IS NOT SAID TO WORK HERE IS CONSIDERED AS BUGGED
    Alpha Release, hey !
    
     * No LUA error
     * Tooltips are fine
     * Setting Menu is quit good too
     * Buff / Curse / Pet Menu nearly functionnal
     * Mounts ok

---
+++ tags/3.1 ALPHA !!!/Bindings.lua
@@ -0,0 +1,164 @@
+﻿--[[
+    Necrosis LdC
+    Copyright (C) 2005-2008  Lom Enfroy
+
+    This file is part of Necrosis LdC.
+
+    Necrosis LdC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Necrosis LdC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Necrosis LdC; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--]]
+
+
+------------------------------------------------------------------------------------------------------
+-- Necrosis LdC
+-- Par Lomig (Kael'Thas EU/FR) & Tarcalion (Nagrand US/Oceanic) 
+-- Contributions deLiadora et Nyx (Kael'Thas et Elune EU/FR)
+--
+-- Skins et voix Françaises : Eliah, Ner'zhul
+--
+-- Version Allemande par Geschan
+-- Version Espagnole par DosS (Zul’jin)
+-- Version Russe par Komsomolka
+--
+-- Version $LastChangedDate$
+------------------------------------------------------------------------------------------------------
+
+
+-- On définit G comme étant le tableau contenant toutes les frames existantes.
+local _G = getfenv(0)
+
+-- Création des raccourcis claviers
+function Necrosis:BindName()
+
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Première section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	-- Le Titre
+	_G["BINDING_HEADER_Necrosis"] = "Necrosis LdC"
+
+	-- La Sphere
+	_G["BINDING_NAME_CLICK NecrosisButton:LeftButton"] = GetSpellInfo(self.Spell[NecrosisConfig.MainSpell].Id)
+
+	-- mounts || Le Cheval
+	if (NecrosisConfig.LeftMount) then
+		_G["BINDING_NAME_CLICK NecrosisMountButton:LeftButton"] = GetSpellInfo(NecrosisConfig.LeftMount)
+	else
+		_G["BINDING_NAME_CLICK NecrosisMountButton:LeftButton"] = GetSpellInfo(self.Spell[1].Id)
+	end
+	
+	if (NecrosisConfig.RightMount) then
+		_G["BINDING_NAME_CLICK NecrosisMountButton:RightButton"] = GetSpellInfo(NecrosisConfig.RightMount)
+	else
+		_G["BINDING_NAME_CLICK NecrosisMountButton:RightButton"] = GetSpellInfo(self.Spell[2].Id)
+	end
+
+	-- La Pierre de feu
+	_G["BINDING_NAME_CLICK NecrosisFirestoneButton:RightButton"] = GetSpellInfo(self.Spell[54].Id)
+	_G["BINDING_NAME_CLICK NecrosisFirestoneButton:LeftButton"] = self.Translation.Item.Firestone
+
+	--La Pierre de sort
+	_G["BINDING_NAME_CLICK NecrosisSpellstoneButton:RightButton"] = GetSpellInfo(self.Spell[53].Id)
+	_G["BINDING_NAME_CLICK NecrosisSpellstoneButton:LeftButton"] = self.Translation.Item.Spellstone
+
+	--La Pierre de soin
+	_G["BINDING_NAME_CLICK NecrosisHealthstoneButton:RightButton"] = GetSpellInfo(self.Spell[52].Id)
+	_G["BINDING_NAME_CLICK NecrosisHealthstoneButton:LeftButton"] = self.Translation.Item.Healthstone.." - "..self.Localize["Utilisation"]
+	_G["BINDING_NAME_CLICK NecrosisHealthstoneButton:MiddleButton"] = self.Translation.Item.Healthstone.." - "..self.Localize["Echange"]
+
+	-- La Pierre de sort
+	_G["BINDING_NAME_CLICK NecrosisSoulstoneButton:RightButton"] = GetSpellInfo(self.Spell[51].Id)
+	_G["BINDING_NAME_CLICK NecrosisSoulstoneButton:LeftButton"] = self.Translation.Item.Soulstone
+	
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Deuxième section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	_G["BINDING_HEADER_NecrosisSpell"] = "Necrosis - "..self.Config.Menus["Menu des Buffs"]
+	
+	_G["BINDING_NAME_CLICK NecrosisBuffMenuButton:LeftButton"] = self.Config.Menus["Menu des Buffs"]
+	
+	local buffID = {31, -- demon armor
+	                47, -- fel armor 
+	                32, -- unending breath
+	                34, -- eye of killrogg
+	                37, -- ritual of summoning
+	                38, -- soul link
+	                43, -- shadow ward
+	                24} -- demonic empowerment
+	for i = 1, #buffID, 1 do
+		_G["BINDING_NAME_CLICK NecrosisBuffMenu"..i..":LeftButton"] = GetSpellInfo(self.Spell[ buffID[i] ].Id)
+	end
+	
+	_G["BINDING_NAME_CLICK NecrosisBuffMenu10:LeftButton"] = GetSpellInfo(self.Spell[9].Id)
+	_G["BINDING_NAME_CLICK NecrosisBuffMenu10:RightButton"] = GetSpellInfo(self.Spell[9].Id).." ("..self.Translation.Misc["Rank"].." 1)"
+
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Troisième section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	_G["BINDING_HEADER_NecrosisPet"] = "Necrosis - "..self.Config.Menus["Menu des Demons"]
+	
+	_G["BINDING_NAME_CLICK NecrosisPetMenuButton:LeftButton"] = self.Config.Menus["Menu des Demons"]
+	
+	for i = 2, 6, 1 do
+		_G["BINDING_NAME_CLICK NecrosisPetMenu"..i..":LeftButton"] = GetSpellInfo(self.Spell[i+1].Id)
+	end
+	
+	local buttonID = {1, 7, 8, 9, 10}
+	local BuffID = {15, -- fel domination
+	                8,  -- summon infernal
+	                30, -- ritual of doom
+	                35, -- enslave demon
+	                44} -- demonic sacrifice
+	for i = 1, #buttonID, 1 do
+		_G["BINDING_NAME_CLICK NecrosisPetMenu"..buttonID[i]..":LeftButton"] = GetSpellInfo(self.Spell[ BuffID[i] ].Id)
+	end
+
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Quatrième section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	-- Le Titre
+	_G["BINDING_HEADER_NecrosisCurse"] = "Necrosis - "..self.Config.Menus["Menu des Maledictions"]
+	
+	_G["BINDING_NAME_CLICK NecrosisCurseMenuButton:LeftButton"] = self.Config.Menus["Menu des Maledictions"]
+	
+	local buffID = {23, -- curse of weakness
+	                22, -- curse of agony
+	                25, -- curse of tongues
+	                40, -- curse of exhaustion
+	                26, -- curse of the elements
+	                16, -- curse of doom
+	                14} -- corruption
+	for i = 1, #buffID, 1 do
+		_G["BINDING_NAME_CLICK NecrosisCurseMenu"..i..":LeftButton"] = GetSpellInfo(self.Spell[ buffID[i] ].Id)
+	end
+
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Cinquième section
+	-------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	-- Le Titre
+	_G["BINDING_HEADER_NecrosisMetamorphosis"] = "Necrosis - "..GetSpellInfo(self.Spell[27].Id)
+	
+	_G["BINDING_NAME_CLICK NecrosisMetamorphosisButton:LeftButton"] = GetSpellInfo(self.Spell[27].Id)
+	
+	local buffID = {39, -- Demon charge
+			24, -- Demonic Empowerment
+			33} -- Bon démoniaque
+	for i = 1, #buffID, 1 do
+		_G["BINDING_NAME_CLICK NecrosisMetamorphosisMenu"..i..":LeftButton"] = GetSpellInfo(self.Spell[ buffID[i] ].Id)
+	end
+end
---
+++ tags/3.1 ALPHA !!!/Spells.lua
@@ -0,0 +1,253 @@
+﻿--[[
+    Necrosis LdC
+    Copyright (C) 2005-2008  Lom Enfroy
+
+    This file is part of Necrosis LdC.
+
+    Necrosis LdC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Necrosis LdC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Necrosis LdC; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--]]
+
+------------------------------------------------------------------------------------------------------
+-- Necrosis LdC
+-- Par Lomig (Kael'Thas EU/FR) & Tarcalion (Nagrand US/Oceanic) 
+-- Contributions deLiadora et Nyx (Kael'Thas et Elune EU/FR)
+--
+-- Skins et voix Françaises : Eliah, Ner'zhul
+--
+-- Version Allemande par Geschan
+-- Version Espagnole par DosS (Zul’jin)
+-- Version Russe par Komsomolka
+--
+-- Version $LastChangedDate$
+------------------------------------------------------------------------------------------------------
+
+local new, del
+do
+	local cache = setmetatable({}, {__mode='k'})
+	function new(populate, ...)
+		local tbl
+		local t = next(cache)
+		if ( t ) then
+			cache[t] = nil
+			tbl = t
+		else
+			tbl = {}
+		end
+		if ( populate ) then
+			local num = select("#", ...)
+			if ( populate == "hash" ) then
+				assert(math.fmod(num, 2) == 0)
+				local key
+				for i = 1, num do
+					local v = select(i, ...)
+					if not ( math.fmod(i, 2) == 0 ) then
+						key = v
+					else
+						tbl[key] = v
+						key = nil
+					end
+				end
+			elseif ( populate == "array" ) then
+				for i = 1, num do
+					local v = select(i, ...)
+					table.insert(tbl, i, v)
+				end
+			end
+		end
+		return tbl
+	end
+	function del(t)
+		for k in next, t do
+			t[k] = nil
+		end
+		cache[t] = true
+	end
+end
+
+
+-- Fonction pour relocaliser  automatiquemlent des éléments en fonction du client
+function Necrosis:SpellLocalize(tooltip) 
+
+	------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Relocalisation des Sorts
+	------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	if not tooltip then
+		self.Spell = {
+			[1] = {Id = 5784,	Length = 0,	Type = 0}, -- Felsteed
+			[2] = {Id = 23161,	Length = 0,	Type = 0}, -- Dreadsteed
+			[3] = {Id = 688,	Length = 0,	Type = 0}, -- Imp || Diablotin
+			[4] = {Id = 697,	Length = 0,	Type = 0}, -- Voidwalker || Marcheur
+			[5] = {Id = 712,	Length = 0,	Type = 0}, -- Succubus || Succube
+			[6] = {Id = 691,	Length = 0,	Type = 0}, -- Fellhunter
+			[7] = {Id = 30146,	Length = 0,	Type = 0}, -- Felguard
+			[8] = {Id = 1122,	Length = 600,	Type = 3}, -- Infernal
+			[9] = {Id = 710,	Length = 30,	Type = 2}, -- Banish
+			[10] = {Id = 1098,	Length = 300,	Type = 2}, -- Enslave
+			[11] = {Id = 27239,	Length = 900,	Type = 1}, -- Soulstone Resurrection || Résurrection de pierre d'ame
+			[12] = {Id = 47811,	Length = 15,	Type = 6}, -- Immolate
+			[13] = {Id = 6215,	Length = 20,	Type = 6}, -- Fear
+			[14] = {Id = 172,	Length = 18,	Type = 5}, -- Corruption
+			[15] = {Id = 18708,	Length = 180,	Type = 3}, -- Fel Domination || Domination corrompue
+			[16] = {Id = 603,	Length = 60,	Type = 3}, -- Bane of Doom || Plaie funeste
+			[17] = {Id = 47847,	Length = 20,	Type = 3}, -- Shadowfury || Furie de l'ombre
+			[18] = {Id = 47825,	Length = 60,	Type = 3}, -- Soul Fire || Feu de l'âme
+			[19] = {Id = 47860,	Length = 120,	Type = 3}, -- Death Coil || Voile mortel
+			[20] = {Id = 47827,	Length = 15,	Type = 3}, -- Shadowburn || Brûlure de l'ombre
+			[21] = {Id = 17962,	Length = 10,	Type = 3}, -- Conflagration
+			[22] = {Id = 980,	Length = 24,	Type = 4}, -- Bane of Agony || Plaie Agonie
+			[23] = {Id = 702,	Length = 120,	Type = 4}, -- Curse of Weakness || Malédiction Faiblesse
+			[24] = {Id = 47193,	Length = 60,	Type = 3}, -- Demonic Empowerment || Renforcement démoniaque
+			[25] = {Id = 1714,	Length = 30,	Type = 4}, -- Curse of Tongues || Malédiction Langage
+			[26] = {Id = 1490,	Length = 300,	Type = 4}, -- Curse of the Elements || Malédiction Eléments
+			[27] = {Id = 59673,	Length = 180,	Type = 3}, -- Metamorphosis || Metamorphose
+			[28] = {Id = 47862,	Length = 30,	Type = 6}, -- Siphon Life || Syphon de vie
+			[29] = {Id = 17928,	Length = 40,	Type = 3}, -- Howl of Terror || Hurlement de terreur
+			[30] = {Id = 18540,	Length = 1800,	Type = 3}, -- Ritual of Doom || Rituel funeste
+			[31] = {Id = 687,	Length = 0,	Type = 0}, -- Demon Armor || Armure démoniaque
+			[32] = {Id = 5697,	Length = 600,	Type = 2}, -- Unending Breath || Respiration interminable
+			[33] = {Id = 54785,	Length = 45,	Type = 3}, -- Bond démoniaque
+			[34] = {Id = 126,	Length = 0,	Type = 0}, -- Eye of Kilrogg
+			[35] = {Id = 1098,	Length = 0,	Type = 0}, -- Enslave Demon
+			[36] = {Id = 696,	Length = 0,	Type = 0}, -- Demon Skin || Peau de démon
+			[37] = {Id = 698,	Length = 120,	Type = 3}, -- Ritual of Summoning || Rituel d'invocation
+			[38] = {Id = 19028,	Length = 0,	Type = 0}, -- Soul Link || Lien spirituel
+			[39] = {Id = 54785,	Length = 45,	Type = 3}, -- Demon Charge || Charge démoniaque
+			[40] = {Id = 18223,	Length = 12,	Type = 4}, -- Curse of Exhaustion || Malédiction de fatigue
+			[41] = {Id = 57946,	Length = 40,	Type = 2}, -- Life Tap || Connexion
+			[42] = {Id = 59164,	Length = 12,	Type = 2}, -- Haunt || Hanter
+			[43] = {Id = 6229,	Length = 30,	Type = 3}, -- Shadow Ward || Gardien de l'ombre
+			[44] = {Id = 18788,	Length = 60,	Type = 3}, -- Sacrifice || Sacrifice démoniaque
+			[45] = {Id = 47809,	Length = 0,	Type = 0}, -- Shadow Bolt
+			[46] = {Id = 47843,	Length = 18,	Type = 6}, -- Unstable Affliction || Affliction instable
+			[47] = {Id = 28176,	Length = 0,	Type = 0}, -- Fel Armor || Gangrarmure
+			[48] = {Id = 47836,	Length = 18,	Type = 5}, -- Seed of Corruption || Graine de Corruption
+			[49] = {Id = 29858,	Length = 180,	Type = 3}, -- SoulShatter || Brise âme
+			[50] = {Id = 29893,	Length = 300,	Type = 3}, -- Ritual of Souls || Rituel des âmes
+			[51] = {Id = 47884,	Length = 0,	Type = 0}, -- Create Soulstone || Création pierre d'âme
+			[52] = {Id = 47878,	Length = 0,	Type = 0}, -- Create Healthstone || Création pierre de soin
+			[53] = {Id = 47888,	Length = 0,	Type = 0}, -- Create Spellstone || Création pierre de sort
+			[54] = {Id = 60220,	Length = 0,	Type = 0}, -- Create Firestone || Création pierre de feu
+			[55] = {Id = 59092,	Length = 0,	Type = 0}, -- Dark Pact || Pacte noir
+			[56] = {Id = 50589,	Length = 30,	Type = 3}, -- Immolation Aura || Aura d'immolation
+		}
+		-- Type 0 = Pas de Timer || no timer
+		-- Type 1 = Timer permanent principal || Standing main timer
+		-- Type 2 = Timer permanent || main timer
+		-- Type 3 = Timer de cooldown || cooldown timer
+		-- Type 4 = Timer de malédiction || curse timer
+		-- Type 5 = Timer de corruption || corruption timer
+		-- Type 6 = Timer de combat || combat timer
+
+		for i in ipairs(self.Spell) do
+			self.Spell[i].Rank = " "
+		end
+	end
+	
+	------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Relocalisation des Tooltips
+	------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
+	-- stones || Pierres
+	local buttonTooltip = new("array",
+		"Soulstone",
+		"Healthstone",
+		"Spellstone",
+		"Firestone"
+	)
+	local colorCode = new("array",
+		"|c00FF99FF", "|c0066FF33", "|c0099CCFF", "|c00FF4444"
+	)
+	for i, button in ipairs(buttonTooltip) do
+		if not self.TooltipData[button] then
+			self.TooltipData[button] = {}
+		end
+		self.TooltipData[button].Label = colorCode[i]..self.Translation.Item[button].."|r"
+	end
+	del(buttonTooltip)
+	del(colorCode)
+	
+	-- Buffs
+	local buttonTooltip = new("array",
+		"Domination",
+		"Enslave",
+		"Armor",
+		"FelArmor",
+		"Aqua",
+		"Kilrogg",
+		"Banish",
+		"TP",
+		"RoS",
+		"SoulLink",
+		"ShadowProtection",
+		"Renforcement"
+	)
+	local buttonName = new("array",
+		15, 35, 31, 47, 32, 34, 9, 37, 50, 38, 43, 24
+	)
+	for i, button in ipairs(buttonTooltip) do
+		if not self.TooltipData[button] then
+			self.TooltipData[button] = {}
+		end
+		self.TooltipData[button].Label = "|c00FFFFFF"..GetSpellInfo(self.Spell[buttonName[i]].Id).."|r"
+	end
+	del(buttonTooltip)
+	del(colorCode)
+	del(buttonName)
+	
+	-- Demons
+	local buttonTooltip = new("array",
+		"Sacrifice",
+		"Metamorphosis",
+		"Charge",
+		"Immolation",
+		"Leap",
+		"Renforcement",
+		"Enslave"
+	)
+	local buttonName = new("array",
+		44, 27, 39, 56, 33, 24, 35
+	)
+	for i, button in ipairs(buttonTooltip) do
+		if not self.TooltipData[button] then
+			self.TooltipData[button] = {}
+		end
+		self.TooltipData[button].Label = "|c00FFFFFF"..GetSpellInfo(self.Spell[buttonName[i]].Id).."|r"
+	end
+	del(buttonTooltip)
+	del(colorCode)
+	del(buttonName)
+	
+	-- Curses || Malédiction
+	local buttonTooltip = new("array",
+		"Weakness",
+		"Agony",
+		"Tongues",
+		"Exhaust",
+		"Elements",
+		"Doom",
+		"Corruption"
+	)
+	local buttonName = new("array",
+		23, 22, 25, 40, 26, 16, 14
+	)
+	for i, button in ipairs(buttonTooltip) do
+		if not self.TooltipData[button] then
+			self.TooltipData[button] = {}
+		end
+		self.TooltipData[button].Label = "|c00FFFFFF"..GetSpellInfo(self.Spell[buttonName[i]].Id).."|r"
+	end
+	del(buttonTooltip)
+	del(colorCode)
+	del(buttonName)
+end
